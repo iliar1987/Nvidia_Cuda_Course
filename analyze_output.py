f250562d01582da2f5dc007864bdb7216abfbaac
iliar1987 <iliaraysin@gmail.com>
2016-04-29 12:57:01 +0300

    Update analyze_output.py

--- analyze_output.py
+++ analyze_output.py
@@ -5,8 +5,6 @@ Created on Fri Apr 29 00:06:31 2016
 @author: ilia
 """
 
-import os
-os.chdir('/home/ilia/Source/CudaCourse/Problem Sets/Problem Set 6')
 
 import numpy as np
 with open('num_neighbors.txt','r') as f:
